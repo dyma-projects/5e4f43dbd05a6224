865cfda7ca35b28a3e5604da9de6e068a9ef28e6
Pamake <pamake28@gmail.com>
2020-03-01 00:28:45 -0500

    exercise 2 et exercise 3

--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-exercice2',
   templateUrl: './exercice2.component.html',
   styleUrls: ['./exercice2.component.css']
 })
-export class Exercice2Component implements OnInit {
+export class Exercice2Component implements OnInit, AfterViewInit {
   public valeur: string;
+  @ViewChild('f', { static: true }) public el: ElementRef;
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit() {
+      
   }
+
+  ngAfterViewInit() {
+    console.log('Values on ngAfterViewInit():');
+    console.log("valeur:", this.el.nativeElement.value);
+    
+  }
+
+showValeur(){
+  this.valeur = this.el.nativeElement.value;
+}
+
+
 
 }
